36086b04b7dc998df0115a3b845a62e8bc68399b
rick2785 <rjhrabowskie@gmail.com>
2020-10-06 07:20:41 -0500

    Initial Commit

---
+++ ShopUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopUI
+//
+//  Created by RJ Hrabowskie on 10/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopUI/ShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIApp.swift
+//  ShopUI
+//
+//  Created by RJ Hrabowskie on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
